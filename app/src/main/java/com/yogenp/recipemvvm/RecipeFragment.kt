c605e7acd228e86c925364814d657b156fa0142d
Yogen Parekh <yogenparekh569@gmail.com>
2021-03-28 00:03:43 +0000

    Model Setup

--- app/src/main/java/com/yogenp/recipemvvm/RecipeFragment.kt
+++ app/src/main/java/com/yogenp/recipemvvm/RecipeFragment.kt
@@ -5,25 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 
-class RecipeListFragment : Fragment() {
+class RecipeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         return ComposeView(requireContext()).apply {
             setContent {
                 Column(modifier = Modifier.padding(16.dp)) {
@@ -31,15 +27,8 @@ class RecipeListFragment : Fragment() {
                         text = "Recipe List",
                         fontSize = 21.sp
                     )
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    Button(onClick = {
-                        findNavController().navigate(R.id.action_recipeListFragment_to_recipeFragment)
-                    }) {
-                        Text(text = "Check Recipe")
-                    }
                 }
             }
         }
-
     }
 }
